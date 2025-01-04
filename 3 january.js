b2326faf5154ace6ad72ae9a4e84c556769c398d
Soniya955729 <soniyajadodiya@gmail.com>
2025-01-04 16:08:42 +0530

    main

---
+++ 3 january.js
@@ -0,0 +1,12 @@
+// variable:
+//variable is location used to store data 
+//which is easly accessible in future use.
+
+//----var
+ var x=40
+ console.log(x)
+
+ //----let
+    let x=33
+    
+    console.log('output:${n}')
---
+++ 4january.js
@@ -0,0 +1,16 @@
+document.write("hello javascript")
+document.write(`<center><h2>javascript</h2></center`)
+document.write(`<center><h1></h1></center`)
+document.write(`<hr>`)
+
+let num1=50
+let num2=10
+let res=num1+num2
+document.write(`<h1> ${num1} + ${num2} = ${res}</h1>`)
+console.log(res)
+
+
+let n1= Number(prompt("ENTER YOUR NUMBER 1 :"))
+let n2= Number(prompt("ENTER YOUR NUMBER 2 :"))
+let total=n1+n2
+document.write(`Result :${total}`)
